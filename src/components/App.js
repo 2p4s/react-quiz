79dfb39784b3057404e2d256ffec04a3d20f4462
Marc Ahmed <marcahmed@msn.com>
2025-07-14 23:10:10 +0100

    handling new answers, style and score

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from "react";
-import DateCounter from "./DateCounter";
+
 import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
@@ -13,6 +13,8 @@ const initialState = {
   //loading, error, ready, active,finished
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -33,13 +35,25 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer":
+      const question = state.questions.at(state.index); //current question
+
+      return {
+        ...state,
+        answer: action.payload,
+        //add points if the question selected is correct
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points //add points for the specific question
+            : state.points, //or just maintain existing points
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -62,7 +76,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,22 @@
-function Options({ question }) {
+import { type } from "@testing-library/user-event/dist/type";
+
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button key={option} className="btn btn-option">
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          disabled={answer !== null}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
